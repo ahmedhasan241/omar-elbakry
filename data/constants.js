bb473fa5cc31d80e751eeda2981312f8e8ca658e
ahmedhasan241 <ahmed7asan8887@gmail.com>
2024-09-29 10:43:24 +0300

    main

--- data/constants.js
+++ data/constants.js
@@ -153,28 +153,28 @@ export const Projects = [
   ],
 
   },
-//   {
-//     id: 7,
+  {
+    id: ,
 
-//     description: `
-//  <div class="px-2 md:!px-6 shadow-md rounded-lg">
-//    <h2 class="font-bold text-xl mb-4  text-center">Some Of My SALLA RESULTS.</h2>
+    description: `
+ <div class="px-2 md:!px-6 shadow-md rounded-lg">
+   <h2 class="font-bold text-xl mb-4  text-center">Some Of My SALLA RESULTS.</h2>
 
-// </div>
-//     `, 
-//   image: [
+</div>
+    `, 
+  image: [
 // 'https://i.postimg.cc/VkdqP5N9/project-1.png' ,
-// 'https://i.postimg.cc/VvKC3cQH/project-2.png' ,
-// 'https://i.postimg.cc/Bv21JFfF/project-3.png' ,
-// 'https://i.postimg.cc/pLFmBjTS/project-4.png' ,
-// 'https://i.postimg.cc/j5sCKkXV/project-5.png' ,
+'https://i.postimg.cc/VvKC3cQH/project-2.png' ,
+'https://i.postimg.cc/Bv21JFfF/project-3.png' ,
+'https://i.postimg.cc/pLFmBjTS/project-4.png' ,
+'https://i.postimg.cc/j5sCKkXV/project-5.png' ,
 // 'https://i.postimg.cc/wBzjcbnt/project-6.png' ,
 // 'https://i.postimg.cc/wBzjcbnt/project-6.png' ,
-// 'https://i.postimg.cc/RhwMH7G1/project-7.png' ,
-// 'https://i.postimg.cc/wTHtkMJx/project-8.png' ,
-//   ],
+'https://i.postimg.cc/RhwMH7G1/project-7.png' ,
+'https://i.postimg.cc/wTHtkMJx/project-8.png' ,
+  ],
 
-//   },
+  },
 
 
 ]
